e0eec3c9a44977e5cc3986af86495e39818ed28b
Dev Unbelievable <devunbelievable@gmail.com>
2023-08-08 10:47:22 +0200

    commit pour le texte de happy birthday

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
-const touchs = document.querySelector('.key');
+//const touchs = document.querySelector('.key');
+const allKeys = document.querySelectorAll('.key');
+
 
 
 // Write named functions that change the color of the keys below
 function keyPlay(event) {
   event.target.style.backgroundColor = 'black';
 };
-touchs.addEventListener('mouseover', keyPlay);
-
 
 function keyReturn(event) {
   event.target.style.backgroundColor = '';
-}
+};
+
+//new function 
+function note(){
+  keys.forEach(key => {
+    key.addEventListener('mousedown', keyPlay);
+    key.addEventListener('mouseup', keyReturn);
+  });
+
+};
+//note();
 // Write a named function with event handler properties
 
 
@@ -35,13 +45,50 @@ nextThree.hidden = true;
 startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
+nextOne.addEventListener('click', ()=>{
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
 
+
+});
 
 // Write anonymous event handler property and function for the second progress button
-
-
+nextTwo.addEventListener('click', () =>{
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+  document.getElementById('word-five').innerHTML = 'DEAR';
+  document.getElementById('word-six').innerHTML = 'FRI-';
+  lastLyric.style.display = 'inline-block';
+  document.getElementById('letter-note-three').innerHTML = 'G';
+  document.getElementById('letter-note-four').innerHTML = 'E';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('letter-note-six').innerHTML = 'B';
+});
 // Write anonymous event handler property and function for the third progress button
 
+nextThree.addEventListener('click', () =>{
+  startOver.hidden = false;
+  nextThree.hidden = true;
+  document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('word-three').innerHTML = 'BIRTH';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('word-four').innerHTML = 'YOU !';
+  // Change 'letter-note-one' to F
+  document.getElementById('letter-note-one').innerHTML = 'F';
+  document.getElementById('letter-note-two').innerHTML = 'F';
+  document.getElementById('letter-note-three').innerHTML = 'E';
+  document.getElementById('letter-note-four').innerHTML = 'C';
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+  //hide lastlyscris
+  lastLyric.style.display = 'none';
+
+
+});
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
